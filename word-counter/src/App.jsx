fb59c25818b6caf7a48c44707b9e821a1084b88b
Tra-My0104 <tramyminxu@gmail.com>
2023-04-13 11:56:28 +0700

    add

---
+++ word-counter/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Wordcount from './Component/Wordcount';
+
+function App() {
+  return (
+    <div className="App">
+        <Wordcount/>
+    </div>
+  );
+}
+
+export default App;
---
+++ word-counter/src/Component/Wordcount.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+function Wordcount() {
+    const [word,setWordCount] = useState(0);
+    const [letter,setLetterCount] = useState(0);
+    const [paragraph,setParagraphCount] = useState(0);
+    const [text,setText] =useState(" ");
+
+    const handleChange = (e) => {
+        const newText = e.target.value.trim();
+        setText(newText);
+        setWordCount(newText.length);
+        
+          
+
+        const newLetter = newText.split(" ");
+        const filterNewLetter = newLetter.filter((words) => words !== "")
+        setLetterCount(filterNewLetter.length)
+
+  
+    } 
+    
+    const handleChangeParagraph = (e) => {
+        if(e.target.value === " "){
+            setParagraphCount(0)
+        } else{
+            setParagraphCount (e.target.value.split(/\r?\n|\r/).length);
+        }
+    }
+
+    return (
+        <div className='container'>
+            <h1><i class="fa-solid fa-book"></i>Work Counter</h1>
+            <div className='item'>
+            <p><i class="fa-sharp fa-solid fa-pen-nib"></i> Word : {word} </p>
+            <p><i class="fa-sharp fa-solid fa-pen"></i> Letter : {letter}</p>
+            <p><i class="fa-solid fa-copy"></i> Paragraph : {paragraph}</p>
+            </div>
+            <textarea
+             onChange={ (e) => {
+             handleChange (e)
+             handleChangeParagraph (e)
+            }
+            }
+             id="textInput"
+             cols="30"
+             rows="10"
+             placeholder='Enter/Paster text here '
+             > 
+            </textarea>
+            <div className='btn'>
+                <button>Click to Uppercase</button>
+                <button>Click to Lowpercase</button>
+            </div>
+        </div>
+    );
+}
+
+export default Wordcount;
